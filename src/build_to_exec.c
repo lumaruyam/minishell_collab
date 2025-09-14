4d60ea6ba019354c6d8bac6bf0341869c6cf10d0
lumaruyam <lapislazuli3113@gmail.com>
2025-09-14 20:39:27 +0200

    added build_exec

---
+++ src/build_to_exec.c
@@ -0,0 +1,61 @@
+
+#include "../inc/minishell.h"
+
+t_exec	*init_build(void)
+{
+	t_exec	*new;
+
+	new = malloc(sizeof(t_exec));
+	if (!new)
+		return (NULL);
+	new->cmd = NULL;
+	new->args = NULL;
+	new->redirs = NULL;
+	new->next = NULL;
+	new->fd_in = STDIN_FILENO;
+	new->fd_out = STDOUT_FILENO;
+	return (new);
+}
+
+int	bd_handle_pipe(t_exec *exec, t_token *token)
+{
+	if (token->type == PIPE)
+	{
+		exec->next = build_to_exec(token->next);
+		return (FAIL);
+	}
+	return (SUCCESS);
+}
+
+int	bd_handle_cmd(t_exec *exec, t_token *token)
+{
+	if (token->type == CMD)
+	{
+		exec->cmd = ft_strdup(token->value);
+		if (!exec->cmd)
+			return (FAIL);
+	}
+	return (SUCCESS);
+}
+
+t_exec	*build_to_exec(t_token *token)
+{
+	t_exec	*exec;
+
+	exec = init_build();
+	if (!exec)
+		return (NULL);
+	while (token != NULL)
+	{
+		if (bd_handle_pipe(exec, token) == FAIL)
+			break ;
+		if (bd_handle_cmd(exec, token) == FAIL)
+			return (NULL);
+		bd_handle_redirs(exec, token);
+		bd_handle_redirs(exec, token); // have not created this function yet
+		bd_handle_args(exec, token);//have not created this function yet Luli
+		token = token->next;
+	}
+	return (exec);
+}
+
