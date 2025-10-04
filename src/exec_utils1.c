48a038f3c0bd9c610447fadbb93d83815be24ced
lumaruyam <lapislazuli3113@gmail.com>
2025-10-04 20:55:45 +0200

    updated exec_utiles

--- src/exec_utils1.c
+++ src/exec_utils1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec_utils.c                                       :+:      :+:    :+:   */
+/*   exec_utils1.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lulmaruy <lulmaruy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 17:00:59 by lulmaruy          #+#    #+#             */
-/*   Updated: 2025/10/04 19:36:09 by lulmaruy         ###   ########.fr       */
+/*   Updated: 2025/10/04 20:55:05 by lulmaruy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,21 +42,5 @@ void	ft_close(t_shell *content)
 	{
 		exe_close(&(content->default_in));
 		exe_close(&(content->default_out));
-	}
-}
-
-void	set_std(t_shell *content, int mode)
-{
-	if (!mode)
-	{
-		content->default_in = dup(STDIN_FILENO);
-		content->defailt_out = dup(STDOUT_FILENO);
-	}
-	else
-	{
-		dup2(content->default_in, STDIN_FILENO);
-		exe_close(&(content->default_in));
-		dup2(content->default_out, STDOUT_FILENO);
-		exec_close(&(content->default_out));
 	}
 }
