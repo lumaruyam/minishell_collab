a804a40b870c518bb1fb609413846456c19494b5
Samira Koudad <skoudad@f2r6s8.clusters.42paris.fr>
2025-10-12 17:30:48 +0200

    updated exec

--- src/exec.c
+++ src/exec.c
@@ -35,8 +35,8 @@ int	exec(t_shell *content)
 	if (content->ct_exec == 0)
 		return (0);
 	set_std(content, 0);
-	tmp = ctx->exec;
-	if (ctx->ct_exec == 1)
+	tmp = content->exec;
+	if (content->ct_exec == 1)
 	{
 		if (tmp->cmd == NULL && tmp->redirs != NULL)
 			return (handle_single_redir(content, tmp));
