63b8e2cad2ce475dc3e50f823110b19afe91a435
lumaruyam <lapislazuli3113@gmail.com>
2025-10-31 19:18:09 +0100

    fixed still reachable leak

--- src/exec_family.c
+++ src/exec_family.c
@@ -6,7 +6,7 @@
 /*   By: lulmaruy <lulmaruy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 20:07:36 by skoudad           #+#    #+#             */
-/*   Updated: 2025/10/30 18:31:07 by lulmaruy         ###   ########.fr       */
+/*   Updated: 2025/10/31 18:16:57 by lulmaruy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,12 +128,14 @@ void	child_process(t_shell *content, int (*fd)[2], int i, t_exec *tmp)
 		close_all(content->ct_exec - 1, fd);
 		ft_close(content);
 		free_after_process(content, NULL);
+		free_shell(content);
 		exit(exit_code);
 	}
 	exit_code = ft_execution(content, tmp);
 	close_all(content->ct_exec - 1, fd);
 	ft_close(content);
 	free_after_process(content, NULL);
+	free_shell(content);
 	if (exit_code == 127)
 		exit(127);
 	else if (exit_code == 126)
